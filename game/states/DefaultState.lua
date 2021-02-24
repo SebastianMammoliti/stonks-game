d256dba2b8c477a385056f1a0d3541fe24e2a910
Seb <sebastian.mammoliti@hotmail.com.au>
2021-02-24 22:18:45 +1100

    Add logic for the ticker, as well as FPS logic

--- game/states/DefaultState.lua
+++ game/states/DefaultState.lua
@@ -1,10 +1,23 @@
-
 DefaultState = Class{__includes = BaseState}
 
-function DefaultState:init() 
+function DefaultState:init()     
+    player1 = Player(WINDOW_WIDTH/2,  WINDOW_HEIGHT/2, 5)
     camera = Camera()
 end
 function DefaultState:enter() end
 function DefaultState:exit() end
-function DefaultState:update(dt) end
-function DefaultState:render() end
+function DefaultState:update(dt) 
+    if love.keyboard.wasPressed('f1') then
+        -- If f1 is Pressed, go to GameState
+        gStateMachine:change('game') 
+    end
+
+    player1:update()
+
+
+end
+function DefaultState:render() 
+    camera:set()
+    player1:render()
+    camera:unset()
+end
